1efa02f5a4856b1d231b9dbfea9c38987369212b
Ademolaayodelemi <ademolaayodelemi@gmail.com>
2023-12-13 02:21:33 +0000

    modified:   App.js modified:   App.scss added  SocialDeviceTable.js added  socialDeviceTable.scss added  AllCourses.js added  allCourses.scss added  AllStaffTable.js added  allStaffTable.scss added  AllStudentsTable.js added  allStudentsTable.scss deleted: Comments.js deleted:  comments.scss added     CourseWorkTable.js added      courseWorkTable.scss deleted:  DataTable.js deleted:  dataTable.scss added     ExamResultsTable.js added     examResultsTable.scss added  Hostel.js added  hostel.scss added   LecturesDetails.js added   lecturesDetails.scss added   NewApplicantsTable.js added   newApplicantsTable.scss added   Remarks.js added   remarks.scss added   SearchPage.js added   searchPage.scss modified: Sidebar.js deleted:  SmallTable.js deleted:  smallTable.scss added     SocialPlatformTable.js added      socialPlatformTable.scss modified:  statusButton.scss modified:   StudentDetails.js modified:   studentDetails.scss modified:   Home.js modified:   home.scss modified:   Management.js modified:   tableData.js

--- src/pages/management/Management.js
+++ src/pages/management/Management.js
@@ -23,3 +23,44 @@ const Management = () => {
 }
 
 export default Management
+
+// modified:   App.js
+// modified:   App.scss
+// added  SocialDeviceTable.js
+// added  socialDeviceTable.scss
+// added  AllCourses.js
+// added  allCourses.scss
+// added  AllStaffTable.js
+// added  allStaffTable.scss
+// added  AllStudentsTable.js
+// added  allStudentsTable.scss
+// deleted: Comments.js
+// deleted:  comments.scss
+// added     CourseWorkTable.js
+// added      courseWorkTable.scss
+// deleted:  DataTable.js
+// deleted:  dataTable.scss
+// added     ExamResultsTable.js
+// added     examResultsTable.scss
+// added  Hostel.js
+// added  hostel.scss
+// added   LecturesDetails.js
+// added   lecturesDetails.scss
+// added   NewApplicantsTable.js
+// added   newApplicantsTable.scss
+// added   Remarks.js
+// added   remarks.scss
+// added   SearchPage.js
+// added   searchPage.scss
+// modified: Sidebar.js
+// deleted:  SmallTable.js
+// deleted:  smallTable.scss
+// added     SocialPlatformTable.js
+// added      socialPlatformTable.scss
+// modified:  statusButton.scss
+// modified:   StudentDetails.js
+// modified:   studentDetails.scss
+// modified:   Home.js
+// modified:   home.scss
+// modified:   Management.js
+// modified:   tableData.js
